5facd4d79e0a26b0656bf29e87e74ef83b8258df
JhoanCarvajal <jhoans127@outlook.es>
2021-05-28 23:32:11 -0500

    registro de usuario funcionando

---
+++ registration/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from .views import UserCreateView
+
+urlpatterns = [
+    path('signup/', UserCreateView.as_view(), name='signup'), 
+]
---
+++ registration/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+from .forms import UserForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+class UserCreateView(CreateView):
+    model = User
+    template_name = "registration/signup.html"
+    form_class = UserForm
+    success_url = reverse_lazy('registration/login.html')
