1738f249ab914c7b9266f1d0d5e12080ba341720
JhoanCarvajal <jhoans127@outlook.es>
2021-05-30 18:43:01 -0500

    agrege el diseño al actualizar producto

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,32 @@
 from django import forms
+from django.forms import widgets
 from .models import Product
+
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
         fields = ['nombre', 'precio', 'informacion', 'fecha_adquisicion', 'imagen', 'id_usuario']
+
+
+class ProductUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['nombre', 'imagen', 'precio', 'informacion', 'fecha_adquisicion']
+
+        widgets = {
+            'nombre':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre'}),
+            # 'imagen':forms.FileInput(attrs={'class':'form-control', 'style':'height: 70px; padding-top: 38px;', 'placeholder':'Imagen'}),
+            'precio':forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Precio'}),
+            'informacion':forms.Textarea(attrs={'class':'form-control', 'style':'height: 100px;', 'placeholder':'Informacion'}),
+            'fecha_adquisicion':forms.DateInput(format ='%Y-%m-%d', attrs={'type':'date','class':'form-control', 'placeholder':'Fecha de adquisicion'}),
+        }
+        labels = {
+            'nombre':'Nombre',
+            'imagen':'Imagen',
+            'precio':'Precio',
+            'informacion':'Informacion',
+            'fecha_adquisicion':'Fecha de adquisicion',
+        }
+
--- products/views.py
+++ products/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.http import request 
+# ccb
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import DeleteView, UpdateView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from django.urls import reverse_lazy
+
+#requerir login
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, ProductUpdateForm
 
 # Create your views here.
+
+@method_decorator(login_required, name='dispatch')
 class ProductListView(ListView):
     model = Product
 
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context["products"] = Product.objects.filter(id_usuario=self.request.user)
+        return context
 
+
+@method_decorator(login_required, name='dispatch')
 class ProductDetailView(DetailView):
     model = Product
 
 
+@method_decorator(login_required, name='dispatch')
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
@@ -24,15 +39,17 @@ class ProductCreateView(CreateView):
         return reverse_lazy('product_list')
 
 
+@method_decorator(login_required, name='dispatch')
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ['nombre', 'imagen', 'precio', 'informacion', 'fecha_adquisicion']
+    form_class = ProductUpdateForm
     template_name_suffix = '_update_form'
 
     def get_success_url(self):
         return reverse_lazy('product_list')
 
 
+@method_decorator(login_required, name='dispatch')
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('product_list')
