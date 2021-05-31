9f403b0f7c2bb6b2f3d164b9eb42e631663ca881
JhoanCarvajal <jhoans127@outlook.es>
2021-05-30 19:12:50 -0500

    rediccionamiento si esta logueado

--- inventario/settings.py
+++ inventario/settings.py
@@ -133,3 +133,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Auth redirect
 LOGIN_REDIRECT_URL = 'product_list'
 LOGOUT_REDIRECT_URL = 'landing_page'
+
+# codigo para el envio de emails
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+else:
+    EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST="smtp.gmail.com"
+    EMAIL_USE_TLS=True
+    EMAIL_PORT=587
+    EMAIL_HOST_USER="44juanc@gmail.com"
+    EMAIL_HOST_PASSWORD="lcscwijdawyohsmm"
